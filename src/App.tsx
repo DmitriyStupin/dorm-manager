acac7c35f1b90f53ecaacaff0f4da88b940b251c
Dmitry Stupin <dm.stup@mail.ru>
2026-05-10 09:40:41 +0700

    feat: integrate JetBrains Mono font and update Home component styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,27 @@ import Layout from "./components/Layout/Layout.tsx";
 import Requests from "./pages/Requests/Requests.tsx";
 import Rooms from "./pages/Rooms/Rooms.tsx";
 import Students from "./pages/Students/Students.tsx";
+import '@fontsource-variable/jetbrains-mono/wght.css'
+import {createTheme, ThemeProvider} from "@mui/material";
 
 const App = () => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "'JetBrains Mono Variable', monospace"
+    }
+  })
+
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/requests'} element={<Requests />} />
-        <Route path={'/rooms'} element={<Rooms />} />
-        <Route path={'/students'} element={<Students />} />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/requests'} element={<Requests />} />
+          <Route path={'/rooms'} element={<Rooms />} />
+          <Route path={'/students'} element={<Students />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import { Typography } from '@mui/material'
+
 const Home = () => {
   return (
-    <div>
+    <Typography>
       Домашняя страница
-    </div>
+    </Typography>
   )
 }
 
